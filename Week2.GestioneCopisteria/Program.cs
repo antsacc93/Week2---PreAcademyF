7eaee694849f7b9b169cc424ce5ee76f143094bd
Antonia Sacchitella <Antonia.Sacchitella@icubed.it>
2022-03-07 16:42:25 +0100

    esercizio copisteria

---
+++ Week2.GestioneCopisteria/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Week2.GestioneCopisteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continua;
+            do
+            {
+                continua = GestoreCopisteria.Menu();
+            } while (continua);
+        }
+    }
+}
