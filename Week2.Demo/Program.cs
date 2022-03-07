04e1a99efef07c842a8a251bb7eba5e409be3ff8
Antonia Sacchitella <Antonia.Sacchitella@icubed.it>
2022-03-07 12:25:52 +0100

    first commit

---
+++ Week2.Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Week2.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 0;
+            //MyMethod(2, 3, ref x);
+            MyMethod(2, 3, x);
+            Console.WriteLine($"La x vale: {x}");
+            Console.WriteLine("La x vale: " + x);
+            Console.WriteLine("La x vale: {0}", x);
+            x = SommaValori(2, 3);
+            
+        }
+
+        //public static void MyMethod(int a, int b, ref int c)
+        public static void MyMethod(int a, int b, int c)
+        {
+            c = a + b;
+        }
+        public static int MyMethod(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public static int SommaValori(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
