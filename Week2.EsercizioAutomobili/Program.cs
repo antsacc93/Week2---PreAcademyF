3aada59c861aeedcfe924ca98693524333b6bf01
Antonia Sacchitella <Antonia.Sacchitella@icubed.it>
2022-03-09 16:51:23 +0100

    esercizio veicoli

---
+++ Week2.EsercizioAutomobili/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Week2.EsercizioAutomobili.Entities;
+
+namespace Week2.EsercizioAutomobili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veicolo veicoloDaAggiungere = new Veicolo();
+            bool continua = true;
+            while (continua)
+            {
+                int scelta = SchermoMenu();
+
+                switch (scelta)
+                {
+                    case 1:
+                       veicoloDaAggiungere = VeicoliManagement.InserisciVeicolo();
+                        break;
+                    case 2:
+                        VeicoliManagement.StampaVeicolo(veicoloDaAggiungere);
+                        break;
+                    default: //un numero diverso da 0
+                        continua = false;
+                        Console.WriteLine("Arrivederci");
+                        break;
+                }
+            }
+
+        }
+
+        public static int SchermoMenu()
+        {
+            Console.WriteLine("1. Inserisci veicolo");
+            Console.WriteLine("2. Stampa veicolo");
+            Console.WriteLine("Qualsiasi altro valore per uscire");
+            Console.Write("Scelta: >");
+            Int32.TryParse(Console.ReadLine(), out int scelta);
+            return scelta;
+        }
+    }
+}
---
+++ Week2.EsercizioAutomobili/VeicoliManagement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Week2.EsercizioAutomobili.Entities;
+
+namespace Week2.EsercizioAutomobili
+{
+    internal static class VeicoliManagement
+    {
+        internal static Veicolo InserisciVeicolo()
+        {
+            Veicolo veicolo = new Veicolo();
+
+            bool success;
+            Console.WriteLine("Inserisci marca");
+            veicolo.Marca = Console.ReadLine();
+            Console.WriteLine("Inserisci i Kilowatt");
+            veicolo.Kilowatt = VerificaInputIntero();
+            Console.WriteLine("Inserisci anno di immatricolazione");
+            veicolo.AnnoImmatricolazione = VerificaInputIntero();
+            Console.WriteLine("Inserisci prezzo d'acquisto");
+            success = Double.TryParse(Console.ReadLine(), out double costo);
+            while (!success)
+            {
+                Console.WriteLine("Inserisci costo");
+                success = Double.TryParse(Console.ReadLine(), out costo);
+            }
+            veicolo.PrezzoAcquisto = costo;
+            Console.WriteLine("Inserisci tipologia");
+            string tipo = Console.ReadLine();
+            veicolo.Tipologia = tipo;
+
+            return veicolo;
+        }
+
+        public static int VerificaInputIntero()
+        {
+            bool success = Int32.TryParse(Console.ReadLine(), out int value);
+            while (!success)
+            {
+                Console.WriteLine("Inserisci un valore corretto");
+                success = Int32.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+
+        public static void StampaVeicolo(Veicolo veicolo)
+        {
+            Console.WriteLine(veicolo);
+        }
+    }
+}
